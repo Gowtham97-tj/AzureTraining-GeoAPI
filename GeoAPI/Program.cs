2775dda0869efde2db446d9ee168ae3497b2be36
gowtham.thiyada <gowtham.thiyada@pal.tech>
2025-11-25 17:32:09 +0530

    Updated jwt Authority

--- GeoAPI/Program.cs
+++ GeoAPI/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAuthentication("Bearer")
     .AddJwtBearer("Bearer", options =>
     {
-        options.Authority = $"https://login.microsoftonline.com/d5cd194a-63e9-4ec7-8d6a-4361d231373b/v2.0";
+        options.Authority = $"https://login.microsoftonline.com/d5cd194a-63e9-4ec7-8d6a-4361d231373b";
         options.Audience = "api://e41990ae-5972-4358-a11f-7a1f2de0860d";
     });
 
